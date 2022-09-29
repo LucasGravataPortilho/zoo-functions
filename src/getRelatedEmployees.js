d2fe237331cdf86f3d0cae5ef120439de43ec45c
Lucas Portilho <lucasportilho18@gmail.com>
2022-09-29 18:14:41 -0300

    adicionando requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees
+    .filter((ger) => ger.managers.length <= 1).some((pessoa) => pessoa.id === id);
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return data.employees
+    .filter((ger) => ger.managers.find((id) => id === managerId))
+    .map((pessoa) => `${pessoa.firstName} ${pessoa.lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
