afe89a8809e313237858e6bead2cf678d1c36bf7
Lucas Portilho <lucasportilho18@gmail.com>
2022-09-29 17:04:28 -0300

    adicionando requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const pegaAnimais = data.species.filter((especie) => especie.name === animal);
+  return pegaAnimais[0].residents.every((bicho) => bicho.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
