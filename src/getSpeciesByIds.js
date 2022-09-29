1911290ad74c4bf86a9a793124e1df53454bc54f
Lucas Portilho <lucasportilho18@gmail.com>
2022-09-29 16:29:26 -0300

    adicionando requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -4,6 +4,8 @@ function getSpeciesByIds(...ids) {
   if (ids.length === 0) {
     return [];
   }
+  const filtraId = data.species.filter((animal) => ids.find((id) => (animal.id === id)));
+  return filtraId;
 }
 
 module.exports = getSpeciesByIds;
