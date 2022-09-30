fd5660c224ca06d86c67915c12861493a3c6c407
Lucas Portilho <lucasportilho18@gmail.com>
2022-09-29 23:17:13 -0300

    adicionando requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,27 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  let senhor = 0;
+  let adulto = 0;
+  let crianca = 0;
+  entrants.forEach((person) => {
+    if (person.age < 18) { crianca += 1; }
+    if (person.age >= 18 && person.age < 50) { adulto += 1; }
+    if (person.age >= 50) { senhor += 1; }
+  });
+  const obj = { adult: adulto, child: crianca, senior: senhor };
+  return obj;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  let soma = 0;
+  if (entrants == null || Object.keys(entrants).length === 0) { return 0; }
+  const entradas = countEntrants(entrants);
+  const { prices } = data;
+  soma += entradas.adult * prices.adult;
+  soma += entradas.child * prices.child;
+  soma += entradas.senior * prices.senior;
+  return soma;
 }
 
 module.exports = { calculateEntry, countEntrants };
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,7 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('o argumento count deve retornar o valor 4', () => {
+    expect(computeData('count', elephants).toBe(4));
+  });
+});
