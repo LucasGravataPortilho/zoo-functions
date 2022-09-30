f8b3249bb37aecf1229b035aab257034ed7be663
Lucas Portilho <lucasportilho18@gmail.com>
2022-09-29 23:21:37 -0300

    commitando os elephants

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,7 +1,7 @@
 const handlerElephants = require('../src/handlerElephants');
 
 describe('Testes da função HandlerElephants', () => {
-  it('o argumento count deve retornar o valor 4', () => {
-    expect(computeData('count', elephants).toBe(4));
-  });
+  // it('o argumento count deve retornar o valor 4', () => {
+  //   expect(computeData('count', elephants).toBe(4));
+  // });
 });
