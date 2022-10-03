3acd12e5635909bdbef7290464b9995d16de4b3f
Lucas Portilho <lucasportilho18@gmail.com>
2022-10-03 16:30:34 -0300

    adicionando funcao sem param

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,29 @@
 const data = require('../data/zoo_data');
 
+const semParam = () => {
+  let obj = {};
+  obj = {
+    NE: data.species.filter((x) => x.popularity)
+      .filter((y) => y.location.includes('NE')).map((z) => z.name),
+    NW: data.species.filter((x) => x.popularity)
+      .filter((y) => y.location.includes('NW')).map((z) => z.name),
+    SE: data.species.filter((x) => x.popularity)
+      .filter((y) => y.location.includes('SE')).map((z) => z.name),
+    SW: data.species.filter((x) => x.popularity)
+      .filter((y) => y.location.includes('SW')).map((z) => z.name),
+  };
+  return obj;
+};
+
+const includesName = (param) => {
+  let obj = {};
+  const array = [];
+};
+
 function getAnimalMap(options) {
   // seu código aqui
 }
 
 module.exports = getAnimalMap;
+
+console.log(data.species.map((nome) => nome.name));
