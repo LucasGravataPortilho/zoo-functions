f4d307a74733076da9eb1ca4f14088cbe5a1d162
Lucas Portilho <lucasportilho18@gmail.com>
2022-10-03 12:38:26 -0300

    funcao sem param feito

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,32 +1,35 @@
 const data = require('../data/zoo_data');
 
-// const semParam = () => {
-//   const array = [];
-//   data.employees.forEach((pessoa) => {
-//     array.push({
-//       id: pessoa.id,
-//       fullname: `${pessoa.firstName} ${pessoa.lastName}`,
-//       species: pessoa.responsibleFor,
-//       locations: pessoa.responsibleFor.map((cada) => data.species.filter((animal) => animal.id === cada))
-//         .map((bicho) => bicho)
-//     })
-//   })
-// };
+const semParam = () => {
+  const array = [];
+  data.employees.forEach((pessoa) => {
+    array.push({
+      id: pessoa.id,
+      fullName: `${pessoa.firstName} ${pessoa.lastName}`,
+      species: pessoa.responsibleFor.map((cada) => data.species.find((animal) => animal.id === cada))
+        .map((bicho) => bicho.name),
+      locations: pessoa.responsibleFor.map((cada) => data.species.find((animal) => animal.id === cada))
+        .map((bicho) => bicho.location)
+    })
+  })
+  return array;
+};
 
-// const getParam = (param) => {
-//   const getName = data.employees.find((nome) => nome.firstName === param.name);
-//   const getLast = data.employees.find((last) => last.lastName === param.name);
-//   const getId = data.employees.find((pessoa) => pessoa.id === param.id);
-//   const obj = {};
-//   if (param === getName || param === getLast || param === getId) {
-//     obj[param] = {
-//       id:
-//     };
-//   }
-// };
+const getParam = (param) => {
+  const getName = data.employees.find((nome) => nome.firstName === param.name);
+  const getLast = data.employees.find((last) => last.lastName === param.name);
+  const getId = data.employees.find((pessoa) => pessoa.id === param.id);
+  const obj = {};
+  if (param === getName || param === getLast || param === getId) {
+    obj[param] = {
+      id:
+    };
+  }
+};
 
 function getEmployeesCoverage() {
-  // seu código aqui
+  return semParam();
 }
 
 module.exports = getEmployeesCoverage;
+
