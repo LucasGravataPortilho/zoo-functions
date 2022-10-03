7ae73f601270f5a8b532617eb7bd4688ca45461b
Lucas Portilho <lucasportilho18@gmail.com>
2022-10-03 12:09:07 -0300

    FINALMENTE adicionando requisito 9

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,5 +1,30 @@
 const data = require('../data/zoo_data');
 
+// const semParam = () => {
+//   const array = [];
+//   data.employees.forEach((pessoa) => {
+//     array.push({
+//       id: pessoa.id,
+//       fullname: `${pessoa.firstName} ${pessoa.lastName}`,
+//       species: pessoa.responsibleFor,
+//       locations: pessoa.responsibleFor.map((cada) => data.species.filter((animal) => animal.id === cada))
+//         .map((bicho) => bicho)
+//     })
+//   })
+// };
+
+// const getParam = (param) => {
+//   const getName = data.employees.find((nome) => nome.firstName === param.name);
+//   const getLast = data.employees.find((last) => last.lastName === param.name);
+//   const getId = data.employees.find((pessoa) => pessoa.id === param.id);
+//   const obj = {};
+//   if (param === getName || param === getLast || param === getId) {
+//     obj[param] = {
+//       id:
+//     };
+//   }
+// };
+
 function getEmployeesCoverage() {
   // seu código aqui
 }
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -5,28 +5,29 @@ const { hours } = data;
 const semParam = () => {
   const dias = Object.keys(hours);
   const horarios = Object.values(hours);
-  const obj = [];
+  const obj = {};
   dias.forEach((dia, index) => {
-    obj.push({
-      [dia]: {
+    if (dia === 'Monday') {
+      obj[dia] = { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
+    } else {
+      obj[dia] = {
         officeHour: `Open from ${horarios[index].open}am until ${horarios[index].close}pm`,
         exhibition: data.species.filter((disponivel) => disponivel.availability.includes(dia))
           .map((nome) => nome.name),
-      },
-    });
+      };
+    }
   });
-  obj[obj.length - 1].Monday.officeHour = 'CLOSED';
-  obj[obj.length - 1].Monday.exhibition = 'The zoo will be closed!';
   return obj;
 };
 
 const comParam = (param) => {
-  const dados = Object.entries(hours);
-  const findDay = dados.find((dia) => dia[0] === param);
   const obj = {};
+  const diasSemana = Object.keys(data.hours);
   if (param === 'Monday') {
-    obj[param] = { officeHour: 'CLOSED', exhibition: 'The zoo is closed!' };
-  } else if (findDay) {
+    obj[param] = { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
+  } else if (diasSemana.some((item) => item.includes(param))) {
+    const dados = Object.entries(data.hours).map((dia) => dia);
+    const findDay = dados.find((dia) => dia[0] === param);
     obj[param] = {
       officeHour: `Open from ${findDay[1].open}am until ${findDay[1].close}pm`,
       exhibition: data.species.filter((disponivel) => disponivel.availability.includes(param))
@@ -39,8 +40,13 @@ const comParam = (param) => {
 };
 
 function getSchedule(scheduleTarget) {
+  const namesDays = Object.keys(hours).concat(data.species.map((bicho) => bicho.name));
+  if (scheduleTarget == null || (namesDays.some((x) => x.includes(scheduleTarget))) === false) {
+    return semParam();
+  }
+  return comParam(scheduleTarget);
 }
 
 module.exports = getSchedule;
 
-console.log(comParam('elephants'));
+// console.log(Object.entries(data.hours).map((dia) => dia).find((dia) => dia[0] === 'Wednesday'));
