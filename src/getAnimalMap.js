0fa4c3b31d3dffe30a02cf38e37d794f5d91ce79
Lucas Portilho <lucasportilho18@gmail.com>
2022-10-04 13:52:26 -0300

    adicionando sem erro

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,19 +1,19 @@
 const data = require('../data/zoo_data');
 
-const semParam = () => {
-  let obj = {};
-  obj = {
-    NE: data.species.filter((x) => x.popularity)
-      .filter((y) => y.location.includes('NE')).map((z) => z.name),
-    NW: data.species.filter((x) => x.popularity)
-      .filter((y) => y.location.includes('NW')).map((z) => z.name),
-    SE: data.species.filter((x) => x.popularity)
-      .filter((y) => y.location.includes('SE')).map((z) => z.name),
-    SW: data.species.filter((x) => x.popularity)
-      .filter((y) => y.location.includes('SW')).map((z) => z.name),
-  };
-  return obj;
-};
+// const semParam = () => {
+//   let obj = {};
+//   obj = {
+//     NE: data.species.filter((x) => x.popularity)
+//       .filter((y) => y.location.includes('NE')).map((z) => z.name),
+//     NW: data.species.filter((x) => x.popularity)
+//       .filter((y) => y.location.includes('NW')).map((z) => z.name),
+//     SE: data.species.filter((x) => x.popularity)
+//       .filter((y) => y.location.includes('SE')).map((z) => z.name),
+//     SW: data.species.filter((x) => x.popularity)
+//       .filter((y) => y.location.includes('SW')).map((z) => z.name),
+//   };
+//   return obj;
+// };
 
 // const includesName = (param) => {
 //   let obj = {};
