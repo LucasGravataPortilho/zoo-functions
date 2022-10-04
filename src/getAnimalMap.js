9d9de0aae2a2fa253ce5ac455f00d34d50734aac
Lucas Portilho <lucasportilho18@gmail.com>
2022-10-04 14:07:20 -0300

    adicionando funcao para retornar residentes da especie

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,24 +1,25 @@
 const data = require('../data/zoo_data');
 
-// const semParam = () => {
-//   let obj = {};
-//   obj = {
-//     NE: data.species.filter((x) => x.popularity)
-//       .filter((y) => y.location.includes('NE')).map((z) => z.name),
-//     NW: data.species.filter((x) => x.popularity)
-//       .filter((y) => y.location.includes('NW')).map((z) => z.name),
-//     SE: data.species.filter((x) => x.popularity)
-//       .filter((y) => y.location.includes('SE')).map((z) => z.name),
-//     SW: data.species.filter((x) => x.popularity)
-//       .filter((y) => y.location.includes('SW')).map((z) => z.name),
-//   };
-//   return obj;
-// };
+const semParam = () => {
+  let obj = {};
+  obj = {
+    NE: data.species.filter((x) => x.popularity)
+      .filter((y) => y.location.includes('NE')).map((z) => z.name),
+    NW: data.species.filter((x) => x.popularity)
+      .filter((y) => y.location.includes('NW')).map((z) => z.name),
+    SE: data.species.filter((x) => x.popularity)
+      .filter((y) => y.location.includes('SE')).map((z) => z.name),
+    SW: data.species.filter((x) => x.popularity)
+      .filter((y) => y.location.includes('SW')).map((z) => z.name),
+  };
+  return obj;
+};
 
-// const includesName = (param) => {
-//   let obj = {};
-//   const array = [];
-// };
+const residentsBySpecies = (animal) => {
+  const specie = data.species.find((bicho) => bicho.name === animal);
+  const residentsSpecie = specie.residents.map((bicho) => bicho.name);
+  return residentsSpecie;
+};
 
 function getAnimalMap(options) {
   // seu código aqui
